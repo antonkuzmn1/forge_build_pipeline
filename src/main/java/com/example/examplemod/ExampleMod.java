0a5dd95e1ba132d4422bdf9a6557f2d26ee73bf6
anton <antonkuzmn1@gmail.com>
2026-02-04 00:49:41 +0700

    build test: example

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -34,7 +34,7 @@ import software.bernie.geckolib.GeckoLib;
 public class ExampleMod
 {
     // Define mod id in a common place for everything to reference
-    public static final String MODID = "examplemod";
+    public static final String MODID = "examplemod"; // test
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
     // Create a Deferred Register to hold Blocks which will all be registered under the "examplemod" namespace
